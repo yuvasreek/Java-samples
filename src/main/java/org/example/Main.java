04949595f39590314b38a12ee1af1acde44413f4
Yuvasree Karuppannan <mail2yuvasree@gmail.com>
2024-02-19 14:26:28 +0000

    swap

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int a=10;
+         int a=10,b=20,temp;
+         temp=a;
+         a=b;
+         b=temp;
 
 
-        System.out.println("Hello world!" + a);
+        System.out.println("Hello world!" +a+" "+b);
     }
 }
