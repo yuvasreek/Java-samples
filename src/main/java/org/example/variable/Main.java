f95c07fec25cfb8efc8c55cf1538a076e60883fb
yuvasreek <134144291+yuvasreek@users.noreply.github.com>
2024-02-19 20:30:13 +0000

    Update Main.java

--- src/main/java/org/example/variable/Main.java
+++ src/main/java/org/example/variable/Main.java
@@ -2,9 +2,9 @@ package org.example.variable;
 
 public class Main {
     public static void main(String[] args) {
-        int a=10;
+        int a=10,b=20;
 
 
-        System.out.println("Hello world!" + a);
+        System.out.println("Hello world!" + a + "  "+b);
     }
 }
