7b14cbd514a06c2ba58462a38bf942ea7ca1c99b
Yuvasree Karuppannan <mail2yuvasree@gmail.com>
2024-02-20 10:52:13 +0000

    evenprogram
    Signed-off-by: Yuvasree Karuppannan <mail2yuvasree@gmail.com>

---
+++ Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i=0;i<=10;i++){
+            if(i%2==0){
+                System.out.println("no is even:" +i);
+            }
+            else{
+                System.out.println("no is odd:"+i);
+            }
+        }
+
+
+    }
+}
